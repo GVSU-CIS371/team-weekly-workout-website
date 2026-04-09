fc157787b23d352dc3d5a08a6c78a273a12632a0
NoahNeumann <neumanno@mail.gvsu.edu>
2026-04-09 09:35:30 -0400

    Update firebase.js

--- FitCore/src/firebase.js
+++ FitCore/src/firebase.js
@@ -3,15 +3,17 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "YOUR_KEY",
+  apiKey: "AIzaSyARLBJnGD8t5MjMR0w1d0CZwkXasxbun8g",
   authDomain: "weekly-workout-website.firebaseapp.com",
   projectId: "weekly-workout-website",
   storageBucket: "weekly-workout-website.firebasestorage.app",
-  messagingSenderId: "YOUR_ID",
-  appId: "YOUR_APP_ID"
+  messagingSenderId: "23019660585",
+  appId: "1:23019660585:web:394704e4ddf733f1bd0338",
+  measurementId: "G-6C0QE1YXZQ"
 };
 
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 export const db = getFirestore(app);
 export const auth = getAuth(app);
